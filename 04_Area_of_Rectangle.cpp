c82aee09322e4b7123e5cf1f48932b4f92ac7823
Vishnu0D <143950128+Vishnu0D@users.noreply.github.com>
2025-08-31 18:22:11 +0530

    Create 04_Area_of_Rectangle.cpp

---
+++ 04_Area_of_Rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  double length,breadth,area;
+  cout<<"Enter the length and breadth of the Rectangle : "<<endl;
+  cin>>length>>breadth;
+  
+  area=length*breadth;
+  
+  cout<<"Area of the Rectangle : "<<area<<endl;
+  return 0;
+}
