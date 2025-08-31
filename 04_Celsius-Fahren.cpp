e483ba1c0528a8bb69ae496815816b0f4a6cd763
Vishnu0D <143950128+Vishnu0D@users.noreply.github.com>
2025-08-31 18:31:11 +0530

    Create 04_Celsius-Fahren.cpp

---
+++ 04_Celsius-Fahren.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float celsius,fahren;
+  
+  cout<<"Enter the Temp in celsius : ":
+  cin>>celsius;
+
+  fahren=(celsius*9/5)+32;
+
+  cout<<"The Temp in Fahrenheiht  is : "<<fahren;
+  return 0;
+}
