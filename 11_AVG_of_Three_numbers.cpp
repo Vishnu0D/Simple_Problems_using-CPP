a45ace74294b0a6e6030c02efd3fd13b82c4a9d0
Vishnu0D <143950128+Vishnu0D@users.noreply.github.com>
2025-09-01 22:51:06 +0530

    Update 11_AVG_of_Three_numbers.cpp

--- 11_AVG_of_Three_numbers.cpp
+++ 11_AVG_of_Three_numbers.cpp
@@ -5,7 +5,7 @@ int main()
   float num1,num2,num3,avg;
   cout<<"Enter the 3 numbers : "<<endl;
   cin>>num1>>num2>>num3;
-  
+  avg=(num1+num2+num3)/3;
   cout<<" The average of the 3 numbers is  "<<avg<<endl;
 return 0  
 }
