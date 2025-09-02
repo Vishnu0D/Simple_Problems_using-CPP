a2ae7481cb9ec3d93bce6ac556f7479795d9acef
Vishnu0D <143950128+Vishnu0D@users.noreply.github.com>
2025-09-02 23:11:30 +0530

    Create 22_Sum_of_n_Natural_Numbers.cpp

---
+++ 22_Sum_of_n_Natural_Numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num,sum;
+    cout<<"Enter an Integer "<<endl;
+    cin>>num;
+
+    sum=(num*(num+1))/2;
+    cout<<"THE SUM OF THE NUMBER TILL :"<<num <<"<- IS :" <<sum<<endl;
+    return 0;
+}
