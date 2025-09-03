fe602190ac8c285a61ab15469a121f039f687da9
Vishnu0D <143950128+Vishnu0D@users.noreply.github.com>
2025-09-03 18:41:58 +0000

    Create 28_Grade_based_on_Mark.cpp

---
+++ 28_Grade_based_on_Mark.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    cout << "Enter marks (0-100): ";
+    cin >> marks;
+
+    if (marks >= 90 && marks <= 100) {
+        cout << "Grade: S" << endl;
+    }
+    else if (marks >= 80 && marks <90 ) {
+        cout << "Grade: A" << endl;
+    }
+    else if (marks >= 70 && marks < 80) {
+        cout << "Grade: B" << endl;
+    }
+    else if (marks >= 60 && marks <70) {
+        cout << "Grade: C" << endl;
+    }
+    else if (marks >= 50 && marks < 60) {
+        cout << "Grade: D" << endl;
+    }
+    else if (marks >= 0) {
+        cout << "Grade: Fail" << endl;
+    }
+    else {
+        cout << "Invalid marks entered!" << endl;
+    }
+
+    return 0;
+}
