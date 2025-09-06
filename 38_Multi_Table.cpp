fc8c990398cb80d8095e733efbf4a3326c4ab2f9
Vishnu0D <143950128+Vishnu0D@users.noreply.github.com>
2025-09-06 14:03:23 +0000

    Create 38_Multi_Table.cpp

---
+++ 38_Multi_Table.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter the Number"<<endl;
+    cin>>n;
+    cout<<"The Multiplication Table of "<<n<<endl;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<n<<" x "<<i<<" = "<<i*n<<endl;
+    }
+    return 0;
+}
