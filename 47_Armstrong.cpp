7eb221a0a7b6806266f99b93f68331e49665258a
Vishnu0D <143950128+Vishnu0D@users.noreply.github.com>
2025-09-06 15:17:05 +0000

    Create 47_Armstrong.cpp

---
+++ 47_Armstrong.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num, originalNum, remainder, result = 0, n = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+    originalNum = num;
+    while (originalNum != 0) {
+        originalNum /= 10;
+        ++n;
+    }
+    originalNum = num;
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        int power = 1;
+        for (int i = 0; i < n; ++i)
+            power *= remainder;
+        result += power;
+        originalNum /= 10;
+    }
+    if (result == num)
+        cout << num << " is an Armstrong number." << endl;
+    else
+        cout << num << " is NOT an Armstrong number." << endl;
+    return 0;
+}
