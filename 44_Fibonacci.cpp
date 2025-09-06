d432979af90b62730cc99f151a02b181c7f4d57d
Vishnu0D <143950128+Vishnu0D@users.noreply.github.com>
2025-09-06 14:39:38 +0000

    Create 44_Fibonacci.cpp

---
+++ 44_Fibonacci.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=0,b=1,n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout<<"Fibonacci Series upto  "<<n<<" is : "<<endl;
+        for (int i=1;i<n; i++) {
+        cout<<a<<" ";
+        int next=a+b;
+        a=b;
+        b=next;
+        }
+    return 0;
+}
